024ec76fcc1b9c32b85f5a4e3ffa7b82d1bf3a3c
tianscar <tianscar@protonmail.com>
2022-06-13 08:21:01 +0800

    Update JavaDoc & add native libs

--- j2se/src/test/java/com/tianscar/soundtouchjni/test/J2SETest.java
+++ j2se/src/test/java/com/tianscar/soundtouchjni/test/J2SETest.java
@@ -6,6 +6,8 @@ import com.tianscar.soundtouchjni.SoundTouch;
 public class J2SETest {
 
     public static void main(String[] args) {
+        System.load("/home/tianscar/扩展/文档/IdeaProjects/soundtouchjni/j2se/src/main/resources/linux-amd64/libSoundTouchDLL.so");
+        System.load("/home/tianscar/扩展/文档/IdeaProjects/soundtouchjni/j2se/src/main/resources/linux-amd64/libsoundtouchjni.so");
         SoundTouch soundTouch = new SoundTouch();
         soundTouch.dispose();
         BPMDetector bpmDetector = new BPMDetector(1, 22400);
