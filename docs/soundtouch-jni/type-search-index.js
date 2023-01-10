160bedcbc58070ccd04a4e7cdd6c8659b1be6673
tianscar <tianscar@protonmail.com>
2023-01-10 22:59:17 +0800

    Moved JavaDoc to separate repository

--- docs/soundtouch-jni/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.tianscar.soundtouch","l":"BPMDetector"},{"p":"com.tianscar.soundtouch","l":"SoundTouch"}];updateSearchResults();
